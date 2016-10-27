9ee2196e68dfd93e57d89452d3b37d9b12a3af93
taqpol <njosephchen@gmail.com>
2016-10-26 17:37:20 -0700

    forgot to scrub unit test code and fixed tests

--- unit-9-collections-advanced/lesson-6-introduction-to-dictionaries/tests.py
+++ unit-9-collections-advanced/lesson-6-introduction-to-dictionaries/tests.py
@@ -7,7 +7,7 @@ class BananaDictTest(unittest.TestCase):
         self.assertEqual(banana_dict(1), {'banana': 1})
     
     def test_dictionary_zero(self):
-        self.assertEqual(banana_dict(1), {})
+        self.assertEqual(banana_dict(0), {})
 
     def test_dictionary_three(self):
-        self.assertEqual(banana_dict(3), {'bananabananabanana': 3}
+        self.assertEqual(banana_dict(3), {'banana': 1, 'bananabanana': 2, 'bananabananabanana': 3})
