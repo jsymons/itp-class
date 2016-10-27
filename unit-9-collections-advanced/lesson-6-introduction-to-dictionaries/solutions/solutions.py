36dbaad09f9eb489101656861ce4d7d9f0f72c4c
taqpol <njosephchen@gmail.com>
2016-10-26 17:35:58 -0700

    fixed original intent of assignment

--- unit-9-collections-advanced/lesson-6-introduction-to-dictionaries/solutions/solutions.py
+++ unit-9-collections-advanced/lesson-6-introduction-to-dictionaries/solutions/solutions.py
@@ -1,4 +1,18 @@
 def banana_dict(num):
-    if num < 1:
-        return {}
-    return {'banana'*num : num}
+    return {'banana'*number: number for number in range(1, num+1)}
+
+import unittest
+
+
+class BananaDictTest(unittest.TestCase):
+
+    def test_dictionary_one(self):
+        self.assertEqual(banana_dict(1), {'banana': 1})
+    
+    def test_dictionary_zero(self):
+        self.assertEqual(banana_dict(0), {})
+
+    def test_dictionary_three(self):
+        self.assertEqual(banana_dict(3), {'banana': 1, 'bananabanana': 2, 'bananabananabanana': 3})
+
+unittest.main()
