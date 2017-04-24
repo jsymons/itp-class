656559e67337a4a8f8fdaa18f31dc9e4ffe8ab62
Ivan Zugnoni <zugnoni.ivan@gmail.com>
2017-04-24 12:19:32 -0300

    Move docstrings to readmes

--- unit-7-quiz-1/lesson-1-question-1/main.py
+++ unit-7-quiz-1/lesson-1-question-1/main.py
@@ -1,10 +1,3 @@
 def question_1():
-    """Return the correct answer for the following question.
-    What's the correct data type of the following values: True, False
-    a) Integer
-    b) Boolean
-    c) String
-    d) Collection
-    """
     # Return the correct value.
     pass
--- unit-7-quiz-1/lesson-2-question-2/README.md
+++ unit-7-quiz-1/lesson-2-question-2/README.md
@@ -1,1 +1,8 @@
-# Question 2
+Return the correct answer for the following question.
+What's the final result of the following operation:
+result = True or ("" and False and []) or (1 and "hello world")
+1. []
+2. 1
+3. True
+4. False
+5. "hello world"
