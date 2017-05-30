03bd77fb479ba348ebb555d44795392f21a8529c
jsymons <jason.symons@gmail.com>
2017-05-30 19:20:44 -0400

    Update solution.py

--- unit-4-data-types/lesson-3-return-a-float/solutions/solution.py
+++ unit-4-data-types/lesson-3-return-a-float/solutions/solution.py
@@ -1,4 +1,4 @@
-def return_float(minutes):
+def return_percentage_as_float(minutes):
     result = minutes * 100 / 60
     rounded_result = "{0:.1f}".format(result)
     return float(rounded_result)
